f3997c81da225d227056e9f854fcc215995023af
Liang Li <liangl@meta.com>
2024-10-31 12:56:27 -0700

    Added jetcompose sample in hybrid app

--- HybridSample/app/src/main/java/com/meta/spatial/samples/hybridsample/PancakeActivity.kt
+++ HybridSample/app/src/main/java/com/meta/spatial/samples/hybridsample/PancakeActivity.kt
@@ -11,6 +11,13 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 // default activity
 class PancakeActivity : Activity() {
@@ -30,3 +37,17 @@ class PancakeActivity : Activity() {
     }
   }
 }
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent { App() }
+  }
+}
+
+@Composable
+fun App() {
+  Column { Text("Hello World!", color = Color.Black) }
+}
+
